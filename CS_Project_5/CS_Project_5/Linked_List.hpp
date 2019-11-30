9ba95b7b52e07bad6bde00e1b733caf610d393af
Austin Chayka <austinchayka@gmail.com>
2019-11-29 20:47:50 -0800

    Progress
    Filled out functions

--- CS_Project_5/CS_Project_5/Linked_List.hpp
+++ CS_Project_5/CS_Project_5/Linked_List.hpp
@@ -3,6 +3,7 @@
 
 #include "Node.hpp"
 #include <iostream>
+#include <assert.h>
 
 template <class T>
 class Linked_List {
@@ -10,45 +11,146 @@ class Linked_List {
 private:
 
 	unsigned int length;
-	Node* head;
+	Node<T>* head;
+
+	void split(Node<T>*, Node<T>*, Node<T>*);
+	Node<T>* merge(Node<T>*, Node<T>*);
 
 public:
+
+	Linked_List();
+
+	~Linked_List();
 
 	void print();
 	void clear();
 
 	unsigned int get_length();
 
+	T get(int);
+
 	unsigned int push_front(T);
 	unsigned int push_back(T);
 	unsigned int insert(T, int);
 
-	void sort_ascending();
-	void sort_descending();
+	unsigned int remove(int);
+
+	void sort_ascending(Node<T>* = head);
+	void sort_descending(Node<T>* = head);
 
 };
 
 #endif
 
 template<class T>
+inline void Linked_List<T>::split(Node<T>* start, Node<T>* list_0, Node<T>* list_1) {
+
+	Node<T>* fast = start->next;
+	Node<T>* slow = start;
+
+	while(fast != nullptr) {
+
+		fast = fast->next;
+		if(fast != nullptr) {
+			fast = fast->next;
+			slow = slow->next;
+		}
+
+	}
+
+	list_0 = start;
+	list_1 = slow->next;
+	slow->next = nullptr;
+
+}
+
+template<class T>
+inline Node<T>* Linked_List<T>::merge(Node<T>* list_0, Node<T>* list_1) {
+
+	if(list_0 == nullptr) return list_1;
+	if(list_1 == nullptr) return list_2;
+
+	Node<T>* new_list;
+
+	if(list_0->val <= list_1->val) {
+		new_list = list_0;
+		new_list->next = merge(new_list->next, list_1);
+	} else {
+		new_list = list_1;
+		new_list->next = merge(list_0, new_list->next);
+	}
+
+	return new_list;
+
+}
+
+template<class T>
+inline Linked_List<T>::Linked_List() {
+
+	length = 0;
+	head = nullptr;
+
+}
+
+template<class T>
+inline Linked_List<T>::~Linked_List() {
+
+	if(length > 0) clear();
+
+}
+
+template<class T>
 inline void Linked_List<T>::print() {
 
-	Node* n = head;
+	if(length == 0) {
+		std::cout << "List is empty." << std::endl;
+		return;
+	}
+
+	Node<T>* n = head;
 	while(n != nullptr) {
-		std::cout << n << " ";
-		n = n.next;
+		std::cout << n->val << " ";
+		n = n->next;
 	}
 	std::cout << std::endl;
 
 }
 
 template<class T>
-inline void Linked_List<T>::clear() {}
+inline void Linked_List<T>::clear() {
+
+	while(length > 0) {
+
+		Node<T>* n = head, * temp;
+		for(int i = 1; i < length; i++) {
+			n = n->next;
+		}
+		temp = n;
+		delete temp;
+		--length;
+
+	}
+
+	head = nullptr;
+
+}
 
 template<class T>
 inline unsigned int Linked_List<T>::get_length() {
 
 	return length;
+
+}
+
+template<class T>
+inline T Linked_List<T>::get(int index) {
+
+	assert(index >= 0 && index < length);
+
+	Node<T>* n = head;
+	for(int i = 1; i < index; i++) n = n->next;
+
+	return n->val;
 
 }
 
@@ -65,17 +167,75 @@ inline unsigned int Linked_List<T>::push_front(T val) {
 }
 
 template<class T>
-inline unsigned int Linked_List<T>::push_back(T) {
-	return 0;
+inline unsigned int Linked_List<T>::push_back(T val) {
+
+	if(head == nullptr) {
+		Node<T>* n = new Node<T>;
+		n->val = val;
+		head = n;
+		return ++length;
+	}
+
+	Node<T>* n = head, * new_n = new Node<T>();
+	new_n->val = val;
+	for(int i = 0; i < length - 1; i++) n = n->next;
+	n->next = new_n;
+
+	return ++length;
+
 }
 
 template<class T>
-inline unsigned int Linked_List<T>::insert(T, int) {
-	return 0;
+inline unsigned int Linked_List<T>::insert(T val, int index) {
+
+	assert(index >= 0, index < length);
+
+	if(index == 0) {
+		Node<T>* n = new Node<T>;
+		n->val = val;
+		n->next = head;
+		head = n;
+		return ++length;
+	}
+	
+	Node<T>* n = head, * new_n = new Node<T>;
+	new_n->val = val;
+	for(int i = 1; i < index; i++) n = n->next;
+	new_n->next = n->next;
+	n->next = new_n;
+
+		return ++length;
+
 }
 
 template<class T>
-inline void Linked_List<T>::sort_ascending() {}
+inline unsigned int Linked_List<T>::remove(int index) {
+
+	assert(index >= 0 && index < length);
+
+	if(index == 0) {
+		Node<T>* temp = head;
+		head = head->next;
+		delete temp;
+		return --length;
+	}
+
+	Node<T>* n = head, * temp;
+	for(int i = 1; i < index; i++) n = n->next;
+	temp = n->next;
+	n->next = temp->next;
+	delete temp;
+
+	return --length;
+
+}
 
 template<class T>
-inline void Linked_List<T>::sort_descending() {}
+inline void Linked_List<T>::sort_ascending(Node<T>* start) {
+
+
+
+}
+
+template<class T>
+inline void Linked_List<T>::sort_descending(Node<T>* start) {}
--- CS_Project_5/CS_Project_5/driver.cpp
+++ CS_Project_5/CS_Project_5/driver.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include "Linked_List.hpp"
+#include "util.h"
 
 using namespace std;
 
 int main(int argc, char argv[]) {
 
-	cout << "test" << endl;
+	Linked_List<int> list;
 
+	for(int i = 0; i < 5; i++) list.push_back(i);
+	list.print();
+	cout << "List has length: " << list.get_length() << endl;
+
+	cout << endl;
+	list.push_front(5);
+	list.insert(6, 2);
+	list.print();
+	cout << "List has length: " << list.get_length() << endl;
+
+	cout << endl;
+	list.remove(2);
+	list.print();
+	cout << "List has length: " << list.get_length() << endl;
+
+	cout << endl;
+	int primes = 0;
+	for(int i = 0; i < list.get_length(); i++) if(is_prime(list.get(i))) primes++;
+	cout << "The list contains " << primes << " prime numbers." << endl;
+
+	list.clear();
+
+	cout << endl;
+	list.print();
+	cout << "List has length: " << list.get_length() << endl;
+	
 	return 0;
 
 }
